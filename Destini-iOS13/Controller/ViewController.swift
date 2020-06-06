c1d8f5f3fa7d5b516301c8d8c27a77e248ac3b9c
Lena Porina <porinalena@gmail.com>
2020-06-06 15:16:54 -0700

    create StoryBrain struct;move array stories and var storyNum to the Model; create nextStory method in StoryBrain

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,13 +14,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let stories = [
-        Story(title: "You see a fork in the road", choice1: "Take a left.", choice2: "Take a right."),
-        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-        Story(title:"You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
-    ]
-    var storyNum = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
